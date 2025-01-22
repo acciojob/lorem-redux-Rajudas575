051630a443d7eb96521b8abe4d2af6c2de985325
Rajudas575 <rajudas575@gmail.com>
2025-01-22 20:30:37 +0530

    Update LoremFetcher.js

--- src/components/LoremFetcher.js
+++ src/components/LoremFetcher.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchLorem } from '../redux/actions';
+import { fetchLorem } from './actions';
 
 const LoremFetcher = () => {
   const dispatch = useDispatch();
