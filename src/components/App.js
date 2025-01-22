687de43e4e237e8e137991352a5e0bea77e08e13
Rajudas575 <rajudas575@gmail.com>
2025-01-22 20:14:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,10 @@
-import React from "react";
-import './../styles/App.css';
-// import Menu from "./menu.js";
-// import SmallBox from "./innerDisplay.js";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-class APP extends React.Component {
-  componentWillUpdate() {
-    try {
-      if (this.props.Begin === true) this.Time();
-    } catch (e) {
-      console.log(e);
-    }
+export const fetchLorem = createAsyncThunk('lorem/fetchLorem', async () => {
+  const response = await fetch('https://api.lorem.com/ipsum');
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
   }
-
-  render() {
-    const {
-      Begin,
-      cards,
-      Timer,
-      WhenClick,
-      WhenReset,
-      Collect,
-      Update
-    } = this.props;
-    console.log(Update, Begin, Timer);
-    return (
-      <div>
-        <div>
-          <Menu timer={Timer} WhenReset={WhenReset} />
-        </div>
-
-        <div className="main_game">
-          {cards.map((value, index) => (
-            <SmallBox
-              key={index}
-              src={value.src}
-              index={index}
-              id={value.name}
-              displayer={value.Display}
-              WhenClick={WhenClick}
-              Collect={Collect}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
-
-export default APP;
+  const data = await response.json();
+  return data;
+});
