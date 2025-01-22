8d5f08ab1952f63d88b9196641360609322a038d
Rajudas575 <rajudas575@gmail.com>
2025-01-22 20:29:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import LoremFetcher from './components/LoremFetcher';
+import store from './store';
+import LoremFetcher from './LoremFetcher';
 
 const App = () => {
   return (
